680e198b3f5de12c2c3c5b6344c01e3cbc569d75
morteza <mostajab@in.tum.de>
2014-08-13 07:35:32 +0200

    Adding the Terrain generation class

---
+++ src/Terrain.cpp
@@ -0,0 +1,16 @@
+#include "Terrain3D.h"
+
+Terrain3D::Terrain3D()
+{
+
+}
+
+void Terrain3D::init()
+{
+
+}
+
+Terrain3D::~Terrain3D()
+{
+
+}
---
+++ src/Terrain.h
@@ -0,0 +1,16 @@
+#ifndef __TERRAIN_3D_H__
+#define __TERRAIN_3D_H__
+
+class Terrain3D
+{
+private:
+
+public:
+	Terrain3D();
+
+	void init();
+
+	~Terrain3D();
+};
+
+#endif
